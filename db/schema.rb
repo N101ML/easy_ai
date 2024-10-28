24aa5c99920e72073aa190d46c2ded1d77994014
Mike Lynch <mj.lynch23@gmail.com>
2024-10-28 11:36:22 -0700

    Added steps data type to Renders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_25_210951) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_28_183558) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -83,6 +83,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_25_210951) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "prompt"
+    t.integer "steps"
     t.index ["model_id"], name: "index_renders_on_model_id"
   end
 
